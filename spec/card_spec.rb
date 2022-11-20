6e807d3509212bca4ae7e97fa6f31b5edb71d744
Stefan Filev <sfilev@expediagroup.com>
2022-11-20 12:44:08 +0000

    reducing duplication via a helper method

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,15 @@
 require_relative '../lib/card.rb'
 
 RSpec.describe Card do
-  before do
-    @card = Card.new("Three", "Clubs")
+  def card
+    Card.new("Three", "Clubs")
   end
 
   it 'has a value' do
-    expect(@card.value).to eq("Three")
+    expect(card.value).to eq("Three")
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq("Clubs")
+    expect(card.suit).to eq("Clubs")
   end
 end
