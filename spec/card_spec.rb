108a17d64c0357e2ba0ffe54b2f397a2fd30499f
Stefan Filev <sfilev@expediagroup.com>
2022-11-20 13:44:29 +0000

    Added a custom error message

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Card do
   it 'has a value and that value can change' do
     expect(card.value).to eq('Three')
     card.value = 'Four'
-    expect(card.value).to eq('Four')
+    expect(card.value).to eq('Four'), "ERROR: the value was incorrect. Should be 'Four' and it was #{card.value}"
   end
 
   it 'has a suit' do
